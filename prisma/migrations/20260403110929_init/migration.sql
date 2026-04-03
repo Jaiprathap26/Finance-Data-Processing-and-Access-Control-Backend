be042085873bf7d5175a9226e111f12cf2677d3c
Jaiprathap26 <Jpking2618@gmail.com>
2026-04-03 16:42:54 +0530

    Initial backend setup with Prisma, configuration, and dependencies

---
+++ prisma/migrations/20260403110929_init/migration.sql
@@ -0,0 +1,58 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('VIEWER', 'ANALYST', 'ADMIN');
+
+-- CreateEnum
+CREATE TYPE "RecordType" AS ENUM ('INCOME', 'EXPENSE');
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "role" "Role" NOT NULL DEFAULT 'VIEWER',
+    "isActive" BOOLEAN NOT NULL DEFAULT true,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "financial_records" (
+    "id" TEXT NOT NULL,
+    "amount" DOUBLE PRECISION NOT NULL,
+    "type" "RecordType" NOT NULL,
+    "category" TEXT NOT NULL,
+    "date" TIMESTAMP(3) NOT NULL,
+    "notes" TEXT,
+    "userId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "financial_records_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- CreateIndex
+CREATE INDEX "users_email_idx" ON "users"("email");
+
+-- CreateIndex
+CREATE INDEX "users_role_idx" ON "users"("role");
+
+-- CreateIndex
+CREATE INDEX "financial_records_userId_idx" ON "financial_records"("userId");
+
+-- CreateIndex
+CREATE INDEX "financial_records_type_idx" ON "financial_records"("type");
+
+-- CreateIndex
+CREATE INDEX "financial_records_category_idx" ON "financial_records"("category");
+
+-- CreateIndex
+CREATE INDEX "financial_records_date_idx" ON "financial_records"("date");
+
+-- AddForeignKey
+ALTER TABLE "financial_records" ADD CONSTRAINT "financial_records_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
